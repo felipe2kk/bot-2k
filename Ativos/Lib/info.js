2deebd33af5220b0ab7984d48e90600859092cdc
felipe2kk <76962961+felipe2kk@users.noreply.github.com>
2021-01-04 14:55:06 -0300

    Add files via upload

---
+++ Ativos/Lib/info.js
@@ -0,0 +1,41 @@
+exports.info = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+*${A187}*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+📌STATUS BOT ATIVO: *${aktif}*
+
+         
+╔════════════════════
+║ 𝐒𝐄𝐗𝐎 𝐁𝐔𝐂𝐄𝐓𝐈𝐍𝐇𝐀🤺
+╠════════════════════
+║   *⚠️ATENÇÃO⚠️*
+╠════════════════════
+║*OS CRÉDITOS NAO SÃO MEUS*
+║*APENAS FIZ UMA VERSÃO INSTÁVEL*
+╠════════════════════
+║╭──❉ *INFO* ❉──
+║│➸ *AUTOR*: Higor
+║│➸ *DESIGNER*: Higor
+║│➸ *YOUTUBE*: Dark ツ
+║╰───────────
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│
+║│ _${wa.me/554384551094}_
+║│➸ *YouTube*
+║│ _${nomer}_
+║│➸ *Instagram <https://www.instagram.com/felipef2k>*
+║╰───────────
+║         ${A187}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${A187}
+╠════════════════════
+║ 𝐒𝐄𝐗𝐎 𝐁𝐔𝐂𝐄𝐓𝐈𝐍𝐇𝐀🤺
+╚════════════════════`
+}
